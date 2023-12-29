00453fbc8841c66bae465e49eb86e3fb354d4d4f
Drone CI <murdoc@storm-clan.de>
2023-12-29 08:00:03 +0000

    feat: added custom snippets

--- init.lua
+++ init.lua
@@ -19,3 +19,7 @@ require "custom.autocmds"
 require "custom.mappings"
 require "custom.abbreviations"
 require "custom.options"
+
+vim.filetype.add { extension = { hcl = "terraform" } }
+vim.g.snipmate_snippets_path = "~/.config/nvim/lua/custom/snippets"
+vim.g.vscode_snippets_path = "~/.config/nvim/lua/custom/vscode_snippets"
