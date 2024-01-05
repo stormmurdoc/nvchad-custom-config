36674b9a0a1d264eff1378c834fe6dc4c80a7873
Drone CI <murdoc@storm-clan.de>
2024-01-05 23:03:02 +0000

    chore: added ansiblels

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "bashls", "eslint", "dockerls", "yamlls", "jsonls", "html", "cssls", "tsserver", "clangd" }
+local servers = { "ansiblels", "bashls", "eslint", "dockerls", "yamlls", "jsonls", "html", "cssls", "tsserver", "clangd" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
