284b02967996a828b6f40c324c6caad5aa84dafe
Drone CI <murdoc@storm-clan.de>
2024-01-30 05:58:07 +0000

    chore: changed statusline to default

--- chadrc.lua
+++ chadrc.lua
@@ -13,7 +13,7 @@ M.ui = {
   hl_add = highlights.add,
   transparency = false,
   statusline = {
-    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    theme = "default", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "round",
