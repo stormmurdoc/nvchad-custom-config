2c3f6df62b851ad50af847f3d1d8dfab20fe9a5f
Drone CI <murdoc@storm-clan.de>
2024-02-01 19:28:18 +0000

    feat: add terraform lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers =
-  { "ansiblels", "bashls", "eslint", "dockerls", "yamlls", "jsonls", "html", "cssls", "tsserver", "clangd" }
+  { "ansiblels", "bashls", "eslint", "dockerls", "yamlls", "jsonls", "html", "cssls", "tsserver", "clangd", "tflint", "trivy", "terraformls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup({
@@ -14,5 +14,7 @@ for _, lsp in ipairs(servers) do
   })
 end
 
---
+lspconfig.terraformls.setup({
+  filetypes = {"terraform", "tfvar", "tf"},
+})--
 -- lspconfig.pyright.setup { blabla}
