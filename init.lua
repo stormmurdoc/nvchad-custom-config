737cc199735398b9b6439c9cdfc9f84e79b23bdd
Drone CI <murdoc@storm-clan.de>
2024-03-24 17:44:12 +0000

    feat: center cursor when moving up/down

--- init.lua
+++ init.lua
@@ -23,3 +23,5 @@ require("custom.options")
 vim.filetype.add({ extension = { hcl = "terraform" } })
 vim.g.snipmate_snippets_path = "~/.config/nvim/lua/custom/snippets"
 vim.g.vscode_snippets_path = "~/.config/nvim/lua/custom/vscode_snippets"
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after moving up half-page"})
