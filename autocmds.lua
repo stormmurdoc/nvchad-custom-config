e642aebb05c59d48a376be850ce5e2bd2bf657f0
Drone CI <murdoc@storm-clan.de>
2024-03-24 16:56:32 +0000

    feat: add restore cursor position auto command

--- autocmds.lua
+++ autocmds.lua
@@ -3,6 +3,22 @@
 local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
+-- Restore cursor position
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
 -- set automatic tabstop and so on depending on filetype
 -- python
 autocmd("FileType", {
